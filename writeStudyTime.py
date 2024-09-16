aa7008e83d8bbd3652e0b7288f8abb9f9a4b0e60
nntsugu <nntsugu@gmail.com>
2024-09-16 14:48:41 +0900

    Adopt discord.py(2.4.0) on writeStudyTime.py

--- writeStudyTime.py
+++ writeStudyTime.py
@@ -1,20 +1,24 @@
 from discord.ext import commands
 import discord
 import asyncio
-
 import config
-
 
 intents = discord.Intents.all()
 TOKEN = config.Token
 prefix = "¥"
 
-bot = commands.Bot(command_prefix=prefix,help_command=None,intents=intents)
+# Botのインスタンスを作成
+bot = commands.Bot(command_prefix=prefix, help_command=None, intents=intents)
 
+# 拡張機能を非同期でロードする関数
+async def load_extensions():
+    await bot.load_extension("Cogs.default")
+    await bot.load_extension("Cogs.Studyrecord.entryExit")
 
-bot.load_extension("Cogs.default")
+# メインの非同期関数を作成してBotを実行
+async def main():
+    await load_extensions()  # 拡張機能をロード
+    await bot.start(TOKEN)  # Botを起動
 
-bot.load_extension("Cogs.Studyrecord.entryExit")
-
-
-bot.run(TOKEN)
+# イベントループでメイン関数を実行
+asyncio.run(main())
